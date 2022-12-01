5386b57a1ba90e7a4c87d0b70cdc8b6f1c1bc89a
GrowlyX <growlyxdev@gmail.com>
2022-12-01 12:37:56 -0500

    feat: day1-part2 solution

--- src/Day1.kt
+++ src/Day1.kt
@@ -24,7 +24,23 @@ fun main()
 
     fun part2(input: List<String>): Int
     {
-        return -1
+        val greatest = mutableListOf(0)
+
+        input.forEach {
+            if (it.isEmpty())
+            {
+                greatest += 0
+                return@forEach
+            }
+
+            greatest[greatest.size - 1] =
+                greatest.last() + it.toInt()
+        }
+
+        return greatest
+            .sortedByDescending { it }
+            .take(3)
+            .sum()
     }
 
     println(part1(readInput(1, 1)))

f7bff5f855da65c15cb8d42b855da138e85df805
Subham <growlyxdev@gmail.com>
2022-12-02 13:33:51 -0500

    fix: qaosdoifoiaj

--- src/Day2.kt
+++ src/Day2.kt
@@ -2,30 +2,44 @@
  * @author GrowlyX
  * @since 12/2/2022
  */
-enum class Play(
-    val score: Int,
-    private val destroysEnumConstants: List<String>
-)
+enum class Play(val score: Int)
 {  
-    Scissors(3, listOf(
-        "Paper"
-    )),
+    Paper(2)  Rock(1), Scissors(3);
 
-    Rock(1, listOf(
-        "Scissors"
-    )),
-        
-    Paper(2, listOf(
-        "Rock"
-    ));
-
-    val destroys by lazy {
-        this.destroysEnumConstants.map(Play::valueOf)
-    }
+    fun destroys() = Play.values()
+        .getOrNull(ordinal + 1)
+        ?: Play.Paper
 }
 
 fun main()
 {
+   fun calculateScore(input: List<String>, withPlay: Boolean): Int {
+        val lose = mapOf(1 to 0, 2 to 1, 0 to 2)
+        val won = mapOf(0 to 1, 1 to 2, 2 to 0)
+
+        return input.sumOf { round ->
+            val (op, result) = round.split(" ").map { it[0].code }
+            val opCode = op - 'A'.code
+            val meCode = result - 'X'.code
+
+            val (resultScore, playScore) = if (withPlay) {
+                when {
+                    opCode == meCode -> 3
+                    won[opCode]!! == meCode -> 6
+                    else -> 0
+                } to meCode + 1
+            } else {
+                3 * meCode to when (meCode) {
+                    0 -> lose[opCode]!!
+                    1 -> opCode
+                    else -> won[opCode]!!
+                } + 1
+            }
+
+            resultScore + playScore
+        }
+    }
+
     fun part1(input: List<String>): Int
     {
         val mappings = mutableMapOf(
@@ -52,17 +66,16 @@ fun main()
                 return@forEach
             }
 
-            if (first in second.destroys)
+            if (second.destroys() == first)
             {
-                score += 0
-                println("You won! $first won to $second")
+                println("You lost! $first lost to $second")
                 return@forEach
             }
 
-            if (second in first.destroys)
+            if (first.destroys() == second)
             {
                 score += 6
-                println("You lost! $first lost to $second")
+                println("You won! $first won to $second")
                 return@forEach
             }
 

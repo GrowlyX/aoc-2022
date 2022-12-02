18f51d71a6606744467548e1b890b5104bbb7bf1
Subham <growlyxdev@gmail.com>
2022-12-02 13:46:37 -0500

    fix: fix 2.0

--- src/Day2.kt
+++ src/Day2.kt
@@ -4,7 +4,7 @@
  */
 enum class Play(val score: Int)
 {  
-    Paper(2)  Rock(1), Scissors(3);
+    Paper(2), Rock(1), Scissors(3);
 
     fun destroys() = Play.values()
         .getOrNull(ordinal + 1)
@@ -13,33 +13,6 @@ enum class Play(val score: Int)
 
 fun main()
 {
-   fun calculateScore(input: List<String>, withPlay: Boolean): Int {
-        val lose = mapOf(1 to 0, 2 to 1, 0 to 2)
-        val won = mapOf(0 to 1, 1 to 2, 2 to 0)
-
-        return input.sumOf { round ->
-            val (op, result) = round.split(" ").map { it[0].code }
-            val opCode = op - 'A'.code
-            val meCode = result - 'X'.code
-
-            val (resultScore, playScore) = if (withPlay) {
-                when {
-                    opCode == meCode -> 3
-                    won[opCode]!! == meCode -> 6
-                    else -> 0
-                } to meCode + 1
-            } else {
-                3 * meCode to when (meCode) {
-                    0 -> lose[opCode]!!
-                    1 -> opCode
-                    else -> won[opCode]!!
-                } + 1
-            }
-
-            resultScore + playScore
-        }
-    }
-
     fun part1(input: List<String>): Int
     {
         val mappings = mutableMapOf(

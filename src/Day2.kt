2fa300f0ecb872bfed7142df3089a0806eec47da
Subham <growlyxdev@gmail.com>
2022-12-02 09:04:21 -0500

    feat: prepare for day2

---
+++ src/Day2.kt
@@ -0,0 +1,19 @@
+/**
+ * @author GrowlyX
+ * @since 12/2/2022
+ */
+fun main()
+{
+    fun part1(input: List<String>): Int
+    {
+        return -1
+    }
+
+    fun part2(input: List<String>): Int
+    {
+        return -1
+    }
+
+    println(part1(readInput(2, 1)))
+    println(part2(readInput(1, 2)))
+}

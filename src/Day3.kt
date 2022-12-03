c890554b05f51fb9d9fce48ff99c3e30bf60b4f4
GrowlyX <growlyxdev@gmail.com>
2022-12-02 21:01:04 -0500

    chore: prepare for day3 and add template.kt

---
+++ src/Day3.kt
@@ -0,0 +1,19 @@
+/**
+ * @author GrowlyX
+ * @since 12/3/2022
+ */
+fun main()
+{
+    fun part1(input: List<String>): Int
+    {
+        return -1
+    }
+
+    fun part2(input: List<String>): Int
+    {
+        return -1
+    }
+
+    println(part1(readInput(3, 1)))
+    println(part2(readInput(3, 2)))
+}

a5a4cf0cc0beb5ebaddf443c1fa7f4ccaf5c2024
GrowlyX <growlyxdev@gmail.com>
2022-12-03 10:54:38 -0500

    chore: prepare for day 4

---
+++ src/Day4.kt
@@ -0,0 +1,19 @@
+/**
+ * @author GrowlyX
+ * @since 12/4/2022
+ */
+fun main()
+{
+    fun part1(input: List<String>): Int
+    {
+        return -1
+    }
+
+    fun part2(input: List<String>): Int
+    {
+        return -1
+    }
+
+    println(part1(readInput(4, 1)))
+    println(part2(readInput(4, 2)))
+}

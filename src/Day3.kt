02f4e52fd72cd6d1fc5b10c4d301f4b4df746e0e
GrowlyX <growlyxdev@gmail.com>
2022-12-03 10:44:04 -0500

    feat: complete solution for day3-part1

--- src/Day3.kt
+++ src/Day3.kt
@@ -2,11 +2,44 @@
  * @author GrowlyX
  * @since 12/3/2022
  */
+val aToZ = 'a'..'z'
+val aToZCap = 'A'..'Z'
+
 fun main()
 {
     fun part1(input: List<String>): Int
     {
-        return -1
+        var totalPriority = 0
+
+        input.forEach {
+            // prereq- contains two chunks
+            val chunks = it.chunked(it.length / 2)
+
+            val first = chunks[0]
+            val second = chunks[1]
+
+            var similar = ' '
+
+            for (char in first)
+            {
+                if (second.contains(char))
+                {
+                    similar = char
+                    break
+                }
+            }
+
+            totalPriority += when (true)
+            {
+                aToZ.contains(similar) -> aToZ.indexOf(similar) + 1
+                aToZCap.contains(similar) -> 26 + (aToZCap.indexOf(similar) + 1)
+                else -> throw IllegalArgumentException(
+                    "No match found, is the character a numeric value? ($similar)"
+                )
+            }
+        }
+
+        return totalPriority
     }
 
     fun part2(input: List<String>): Int

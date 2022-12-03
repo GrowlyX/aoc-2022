34cd175302f18e5d5a3f6547db55b4a308385504
GrowlyX <growlyxdev@gmail.com>
2022-12-03 10:53:14 -0500

    feat: complete solution for day3-part2

--- src/Day3.kt
+++ src/Day3.kt
@@ -7,12 +7,21 @@ val aToZCap = 'A'..'Z'
 
 fun main()
 {
+    fun calculatePriority(char: Char) =
+        when (true)
+        {
+            aToZ.contains(char) -> aToZ.indexOf(char) + 1
+            aToZCap.contains(char) -> 26 + (aToZCap.indexOf(char) + 1)
+            else -> throw IllegalArgumentException(
+                "No match found, is the character a numeric value? ($char)"
+            )
+        }
+
     fun part1(input: List<String>): Int
     {
         var totalPriority = 0
 
         input.forEach {
-            // prereq- contains two chunks
             val chunks = it.chunked(it.length / 2)
 
             val first = chunks[0]
@@ -29,14 +38,7 @@ fun main()
                 }
             }
 
-            totalPriority += when (true)
-            {
-                aToZ.contains(similar) -> aToZ.indexOf(similar) + 1
-                aToZCap.contains(similar) -> 26 + (aToZCap.indexOf(similar) + 1)
-                else -> throw IllegalArgumentException(
-                    "No match found, is the character a numeric value? ($similar)"
-                )
-            }
+            totalPriority += calculatePriority(similar)
         }
 
         return totalPriority
@@ -44,7 +46,25 @@ fun main()
 
     fun part2(input: List<String>): Int
     {
-        return -1
+        var totalPriority = 0
+
+        input.chunked(3)
+            .forEach { chunk ->
+                var similar = ' '
+
+                for (char in chunk.first())
+                {
+                    if (chunk.all { it.contains(char) })
+                    {
+                        similar = char
+                        break
+                    }
+                }
+
+                totalPriority += calculatePriority(similar)
+            }
+
+        return totalPriority
     }
 
     println(part1(readInput(3, 1)))

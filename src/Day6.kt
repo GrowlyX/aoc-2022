0460b5244e9b0c3a249e1ffdb2007f616937a6c8
Subham <growlyxdev@gmail.com>
2022-12-06 17:34:28 +0000

    feat: add untested day6-part1 solution

---
+++ src/Day6.kt
@@ -0,0 +1,36 @@
+/**
+ * @author GrowlyX
+ * @since 12/2/2022
+ */
+fun main()
+{
+    fun part1(input: List<String>): Int
+    {
+        val text = input.first()
+        var selected = -2
+
+        for (index in 3..text.length - 1)
+        {
+            val indices = ((index - 3)..index).toList()
+            val mappedToChars = indicies
+                .map { text.charAt(it) }
+                .toSet()
+
+            if (mappedToChars.size == 4)
+            {
+                selected = index
+                break
+            }
+        }
+
+        return selected
+    }
+
+    fun part2(input: List<String>): Int
+    {
+        return -1
+    }
+
+    println(part1(readInput(6, 1)))
+    println(part2(readInput(6, 2)))
+}

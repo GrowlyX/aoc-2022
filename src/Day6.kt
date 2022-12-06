11ebadfb5747a2429a01f3584d8e848dee1b4426
Subham <growlyxdev@gmail.com>
2022-12-06 19:21:59 +0000

    chore: code cleanup for day 6

--- src/Day6.kt
+++ src/Day6.kt
@@ -4,32 +4,32 @@
  */
 fun main()
 {
-    fun parseWithMaxCharIndex(
-        input: List<String>, mci: Int
+    fun parseIntoUniqueCharRanged(
+        input: List<String>, range: Int
     ): Int
     {
-        val text = input.first()
-        var selected = -1
+        val text = input.firstOrNull()
+            ?: return -2
 
-        for (index in mci..text.length - 1)
+        for (index in range..text.length - 1)
         {
-            val indices = ((index - mci)..index).toList()
-            val mappedToChars = indices
-                .map { text[it] }
-                .toSet()
+            val indices = (index - range)..index
+            val characters = indices
+                .distinctBy { text[it] }
 
-            if (mappedToChars.size == mci + 1)
+            if (characters.size == range + 1)
             {
-                selected = index + 1
-                break
+                return index + 1
             }
         }
 
-        return selected
+        return -1
     }
 
-    fun part1(input: List<String>) = parseWithMaxCharIndex(input, 3)
-    fun part2(input: List<String>) = parseWithMaxCharIndex(input, 13)
+    fun part1(input: List<String>) = 
+        parseIntoUniqueCharRanged(input, 3)
+    fun part2(input: List<String>) =
+        parseIntoUniqueCharRanged(input, 13)
 
     println(part1(readInput(6, 1)))
     println(part2(readInput(6, 2)))
